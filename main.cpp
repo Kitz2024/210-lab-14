c1aa1deb0a269db80185e2621a5f54a15ecacf57
Kitz2024 <kit.pollinger@gmail.com>
2024-09-25 10:04:14 -0700

    10AM Commit

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@ public:
     Color(int r = 0, int g = 0, int b = 0) : red(r), green(g), blue(b) {}
 
     int getRed() const { return red; }
-    void setRed(int r) { red = r;}
-    int getGreen() const { return green;}
-    void setGreen(int g) { green = g;}
-    int getBlue() const { return blue;}
+    void setRed(int r) { red = r; }
+    int getGreen() const { return green; }
+    void setGreen(int g) { green = g; }
+    int getBlue() const { return blue; }
     void setBlue(int b) { blue = b; }
 
     void print() const
@@ -21,14 +21,24 @@ public:
         cout << "Red: " << red << ", Green: " << green << ", Blue: " << blue << endl;
     }
 
-    private:
+private:
     int red;
     int green;
     int blue;
 };
 
 int main()
-    {
+{
+    Color colorr(255, 0, 0);
+    Color colorg(0, 255, 0);
+    Color colorb(0, 0, 255);
 
-        return 0;
-    }
+    cout << "Color 1: " << endl;
+    colorr.print();
+    cout << "Color 2: " << endl;
+    colorg.print();
+    cout << "Color 3: " << endl;
+    colorb.print();
+
+    return 0;
+}
